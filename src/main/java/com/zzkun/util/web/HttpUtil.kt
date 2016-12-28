b9f194a8c95d3154979e94792bd935b649a5badb
kun368 <kun368@126.com>
2016-12-28 20:39:52 +0800

    添加爬取Https页面的辅助类，修正vjudge升级后不能爬取数据问题

---
+++ src/main/java/com/zzkun/util/web/HttpUtil.kt
@@ -0,0 +1,73 @@
+package com.zzkun.util.web
+
+import org.apache.commons.io.IOUtils
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.conn.scheme.Scheme
+import org.apache.http.impl.client.BasicResponseHandler
+import org.apache.http.impl.client.DefaultHttpClient
+import org.springframework.stereotype.Component
+
+import java.io.IOException
+import java.net.URL
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+
+/**
+ * 网络抓取工具类
+ * Created by kun36 on 2016/7/4.
+ */
+@Component
+open class HttpUtil {
+
+    /**
+     * 重试机制强力读取Web内容
+     */
+    @Throws(IOException::class)
+    fun readURL(url: String): String {
+        var result: String? = null
+        var exception: IOException? = null
+        for (i in 0..3) {
+            try {
+                result = IOUtils.toString(URL(url), "utf8")
+                if (result != null)
+                    break
+            } catch (e: IOException) {
+                exception = e
+            }
+        }
+        if (result == null)
+            throw exception!!
+        return result
+    }
+
+    fun readHttpsURL(url: String): String {
+        val httpclient = DefaultHttpClient()
+        val ctx = SSLContext.getInstance("SSL")
+        val tm = object : X509TrustManager {
+            override fun checkClientTrusted(p0: Array<out X509Certificate>?, p1: String?) {
+
+            }
+            override fun checkServerTrusted(p0: Array<out X509Certificate>?, p1: String?) {
+
+            }
+            override fun getAcceptedIssuers(): Array<out X509Certificate>? {
+                return null
+            }
+        }
+        ctx.init(null, arrayOf<TrustManager>(tm), null)
+        val ssf = org.apache.http.conn.ssl.SSLSocketFactory(ctx)
+        val ccm = httpclient.getConnectionManager()
+        val sr = ccm.getSchemeRegistry()
+        sr.register(Scheme("https", 443, ssf))
+        val httpget = HttpGet(url)
+        val params = httpclient.getParams()
+        httpget.setParams(params)
+        System.out.println("REQUEST:" + httpget.getURI())
+        val responseHandler = BasicResponseHandler()
+        val responseBody: String
+        responseBody = httpclient.execute(httpget, responseHandler)
+        return responseBody
+    }
+}
--- src/main/java/com/zzkun/util/web/VJudgeWebGetter.kt
+++ src/main/java/com/zzkun/util/web/VJudgeWebGetter.kt
@@ -4,8 +4,10 @@ import com.alibaba.fastjson.JSON
 import com.alibaba.fastjson.JSONObject
 import org.jsoup.Jsoup
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.util.*
+
 
 /**
  * Created by kun on 2016/9/29.
@@ -17,10 +19,13 @@ open class VJudgeWebGetter {
         val logger = LoggerFactory.getLogger(VJudgeWebGetter::class.java)
     }
 
+    @Autowired private lateinit var httpUtil: HttpUtil
+
+
     private fun getUserACJsonDate(vjname: String, link: String): String {
         val url = String.format(link, vjname)
-        val html = Jsoup.connect(url).timeout(7777).get().body()
-        return html?.select("textarea[name=dataJson]")?.text() ?: ""
+        val html = httpUtil.readHttpsURL(url)
+        return Jsoup.parse(html).select("textarea[name=dataJson]")?.text() ?: ""
     }
 
     fun getUserACMap(vjname: String?, link: String): Map<String, List<String>> {
@@ -31,7 +36,7 @@ open class VJudgeWebGetter {
         val res = HashMap<String, List<String>>()
         if(acmap != null) {
             for(key in acmap.keys) {
-                val pbList = arrayListOf<String>()
+                val pbList = ArrayList<String>()
                 val pbs = acmap.getJSONArray(key)
                 for(i in pbs.indices)
                     pbList.add(pbs.getString(i))
