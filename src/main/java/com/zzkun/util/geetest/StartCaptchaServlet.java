596f473ac76f54b63898236969af58f861a1ebf8
kun368 <kun368@126.com>
2018-04-01 19:19:55 +0800

    add travis

--- src/main/java/com/zzkun/util/geetest/StartCaptchaServlet.java
+++ src/main/java/com/zzkun/util/geetest/StartCaptchaServlet.java
@@ -3,43 +3,30 @@ package com.zzkun.util.geetest;
 
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
 /**
  * 使用Get的方式返回challenge和capthca_id,此方式以实现前后端完全分离的开发模式
- *
  */
 @Component
 public class StartCaptchaServlet {
 
-	public void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-
-		GeetestLib gtSdk = new GeetestLib(GeetestConfig.getCaptcha_id(), GeetestConfig.getPrivate_key());
-
-		String resStr = "{}";
-		
-		//自定义userid
-		String userid = "test";
-
-		//进行验证预处理
-		int gtServerStatus = gtSdk.preProcess(userid);
-		
-		//将服务器状态设置到session中
-		request.getSession().setAttribute(gtSdk.gtServerStatusSessionKey, gtServerStatus);
-		//将userid设置到session中
-		request.getSession().setAttribute("userid", userid);
-		
-		resStr = gtSdk.getResponseStr();
-
-		PrintWriter out = response.getWriter();
-		out.println(resStr);
-
-	}
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        GeetestLib gtSdk = new GeetestLib(GeetestConfig.getCaptcha_id(), GeetestConfig.getPrivate_key());
+        String resStr = "{}";
+        //自定义userid
+        String userid = "test";
+        //进行验证预处理
+        int gtServerStatus = gtSdk.preProcess(userid);
+        //将服务器状态设置到session中
+        request.getSession().setAttribute(gtSdk.gtServerStatusSessionKey, gtServerStatus);
+        //将userid设置到session中
+        request.getSession().setAttribute("userid", userid);
+        resStr = gtSdk.getResponseStr();
+        PrintWriter out = response.getWriter();
+        out.println(resStr);
+    }
 }
